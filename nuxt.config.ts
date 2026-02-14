d857c45823ff1502b5ffcabc51ff723bccbefcd9
SatooRu65536 <ryuto65536@gmail.com>
2026-02-14 16:52:36 +0900

    feat: OGP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,17 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'ja',
       },
+      meta: [
+        { name: 'description', content: '愛知工業大学のバス時刻案内を非公式に提供しています。' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: '愛工大バス時刻案内<非公式>' },
+        { property: 'og:description', content: '愛知工業大学のバス時刻案内を非公式に提供しています。' },
+        { property: 'og:image', content: '/ogp.webp' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: '愛工大バス時刻案内<非公式>' },
+        { name: 'twitter:description', content: '愛知工業大学のバス時刻案内を非公式に提供しています。' },
+        { name: 'twitter:image', content: '/ogp.webp' },
+      ],
     },
   },
 
