944483b1dc164958f6312c31951eecb9d6bdd598
SatooRu65536 <ryuto65536@gmail.com>
2026-02-14 15:41:45 +0900

    fix: 日付選択のチラつき

--- app/components/sections/DateSelect.vue
+++ app/components/sections/DateSelect.vue
@@ -7,6 +7,13 @@ const { onDateChange } = defineProps<{
 }>();
 
 const dateModel = defineModel<Date | null>();
+
+const isMounted = ref(false);
+
+onMounted(() => {
+  isMounted.value = true;
+});
+
 const format = 'yyyy年M月d日';
 </script>
 
@@ -15,20 +22,24 @@ const format = 'yyyy年M月d日';
     id="date-select"
     title="日付検索"
   >
-    <ClientOnly>
-      <div class="specified-day">
-        <span for="date-picker">日付検索:</span>
-        <VueDatePicker
-          id="date-picker"
-          v-model="dateModel"
-          :formats="{ input: format, preview: format }"
-          :auto-apply="true"
-          :time-picker="false"
-          :time-config="{ enableTimePicker: false }"
-          @update:model-value="onDateChange"
-        />
+    <div class="specified-day">
+      <span>日付検索:</span>
+      <VueDatePicker
+        v-if="isMounted"
+        v-model="dateModel"
+        :formats="{ input: format, preview: format }"
+        :auto-apply="true"
+        :time-picker="false"
+        :time-config="{ enableTimePicker: false }"
+        @update:model-value="onDateChange"
+      />
+      <div
+        v-else
+        class="date_picker_placeholder"
+      >
+        <CommonCalendarIcon />
       </div>
-    </ClientOnly>
+    </div>
   </LayoutsSection>
 </template>
 
@@ -36,9 +47,32 @@ const format = 'yyyy年M月d日';
 .specified-day {
   margin: 2rem auto 0;
   width: fit-content;
+  height: 3rem;
   display: grid;
   grid-template-columns: auto 1fr;
   align-items: center;
   gap: 1rem;
+}
+
+.date_picker_placeholder {
+  width: 243.5px;
+  height: 38px;
+  border-radius: 4px;
+  border: 1px solid rgb(221, 221, 221);
+  color: var(--dp-text-color);
+  box-sizing: border-box;
+  position: relative;
+
+  svg {
+    box-sizing: content-box;
+    padding: 6px 11px;
+    width: 1rem;
+    height: 1rem;
+    color: rgb(149, 149, 149);
+    position: absolute;
+    top: 50%;
+    inset-inline-start: 0;
+    transform: translateY(-50%);
+  }
 }
 </style>
